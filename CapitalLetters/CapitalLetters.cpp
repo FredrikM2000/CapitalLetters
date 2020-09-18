7186aee509ca8804af5a68057c1517a28ab97231
Meisam Taheri <60437951+Taheri-meisam@users.noreply.github.com>
2020-09-18 01:15:02 -0700

    Add project files.

---
+++ CapitalLetters/CapitalLetters.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+char ChangeToCapital(char);
+char ChangeToCapital(char ch){
+    char cap = ' ';
+    std::string UpdateWord;
+    cap = toupper(ch);
+    return cap;
+}
+
+int main()
+{
+    std::vector <std::string> tempSentence;
+    std::cout << "Please type something:";
+    std::string sentence;
+    std::getline(std::cin, sentence);
+
+    for (int i = 0; i < sentence.size(); i++) {
+       if (i > 1) {
+            if (sentence.at(i - 1) == ' ') {
+                sentence.at(i) = ChangeToCapital(sentence.at(i));
+            }
+        }
+        else if (i == 0) {
+            sentence.at(i) = ChangeToCapital(sentence.at(i));
+        }
+    }
+    std::cout << sentence;
+}
